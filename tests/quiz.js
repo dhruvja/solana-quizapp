71bdf64c1e0c49b17548f86775f1eb764fe6dd29
Dhruv D Jain <dhruv@iamsizzling.com>
2022-04-13 23:27:51 +0530

    added proper test cases

--- tests/quiz.js
+++ tests/quiz.js
@@ -15,9 +15,7 @@ describe("quiz", () => {
   it("Is initialized!", async () => {
     // Add your test here.
 
-    let questions = [ { name: 'hey', rightOption: 0, options: [ 'Wassup' ] }, { name: 'hey bro', rightOption: 1, options: [ 'Wassup', 'cool', 'yo' ] }, { name: 'how are u doing', rightOption: 2, options: [ 'Wassup', 'cool', 'yo', 'sup' ] }  ]
-
-    let quizName = "Test"
+    
 
     let tx = await program.rpc.initialize({
       accounts: {
@@ -28,7 +26,20 @@ describe("quiz", () => {
       signers: [baseAccount]
     });
 
-    console.log("Your transaction signature", tx);
+    // console.log("Your transaction signature", tx);
+
+    const vals = await program.account.quiz.fetch(baseAccount.publicKey);
+
+    assert.ok(vals.totalQuiz.eq(new anchor.BN(0)))
+    
+  });
+
+  it("Able to 1 quizzes", async() => {
+
+    let questions = [ { name: 'hey', rightOption: 0, options: [ 'Wassup' ] }, { name: 'hey bro', rightOption: 1, options: [ 'Wassup', 'cool', 'yo' ] }, { name: 'how are u doing', rightOption: 2, options: [ 'Wassup', 'cool', 'yo', 'sup' ] }  ]
+
+    let quizName = "Test"
+
 
     tx = await program.rpc.addQuiz(quizName, questions, {
       accounts: {
@@ -37,16 +48,27 @@ describe("quiz", () => {
       }
     })
 
-    console.log(tx);
+    tx = await program.rpc.addQuiz("Another", questions, {
+      accounts: {
+        quiz: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+      }
+    })
+
+    // console.log(tx);
 
     const vals = await program.account.quiz.fetch(baseAccount.publicKey);
-    console.log(vals.totalQuiz)
-    console.log(vals.quizList)
-    console.log(vals.quizList[0].questions)
+    // console.log(vals.totalQuiz)
+    // console.log(vals.quizList)
+    // console.log(vals.quizList[0].questions)
 
+    assert.ok(vals.totalQuiz.eq(new anchor.BN(2)))
+  })
+
+  it("Calculates the score", async () => {
     const scores = [0, 1, 0];
 
-    tx = await program.rpc.calculateScore(0, new Buffer(scores),{
+    tx = await program.rpc.calculateScore(0, new Buffer.from(scores),{
       accounts: {
         quiz: baseAccount.publicKey,
         user: provider.wallet.publicKey
@@ -54,9 +76,8 @@ describe("quiz", () => {
     });
 
     const val = await program.account.quiz.fetch(baseAccount.publicKey);
-    console.log(val.quizList[0].scores)
+    // console.log(val.quizList[0].scores[0])
 
-
-    assert.ok(vals.totalQuiz.eq(new anchor.BN(1)))
-  });
+    assert.ok(val.quizList[0].scores[0] == 20)
+  })
 });
